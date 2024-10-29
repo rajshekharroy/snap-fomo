bce70512750127f743b223e019f22e5fd7bedc11
Rajshekhar Roy <raj.codehub@gmail.com>
2024-10-29 10:10:32 +0530

    Add post instead of send

--- components/AddPostButton.tsx
+++ components/AddPostButton.tsx
@@ -15,10 +15,10 @@ const AddPostButton = () => {
       {pending ? (
         <div className="flex items-center gap-2">
           <div className="inline-block h-[10px] w-[10px] animate-spin rounded-full border-2 border-white-300 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite]" />
-          Sending
+          Posting
         </div>
       ) : (
-        "Send"
+        "Post"
       )}
     </button>
   );
